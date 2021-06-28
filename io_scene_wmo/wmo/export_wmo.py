30e4edf3291084c1935be03b09406171fcac728d
intra <intra@tuta.io>
2021-06-28 19:13:28 +0200

    Revert references to WoWVersionManager back to CLIENT_VERSION enum

--- io_scene_wmo/wmo/export_wmo.py
+++ io_scene_wmo/wmo/export_wmo.py
@@ -2,7 +2,7 @@ import bpy
 import time
 
 from ..pywowlib.wmo_file import WMOFile
-from ..pywowlib import WoWVersions
+from ..pywowlib import CLIENT_VERSION
 
 from .wmo_scene import BlenderWMOScene
 
@@ -14,7 +14,7 @@ def export_wmo_from_blender_scene(filepath, export_selected, export_method):
 
     start_time = time.time()
 
-    wmo = WMOFile(WoWVersions, filepath)
+    wmo = WMOFile(CLIENT_VERSION, filepath)
     wmo.export = export_method != 'PARTIAL'
     bl_scene = BlenderWMOScene(wmo, get_addon_prefs())
 
