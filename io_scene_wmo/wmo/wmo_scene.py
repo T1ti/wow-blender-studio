aec60f4843069729efb7b42c0fb60f764f65f805
intra <intra@tuta.io>
2021-08-08 14:21:00 +0200

    wmo: fix ambient color conversion (credits to titi)

--- io_scene_wmo/wmo/wmo_scene.py
+++ io_scene_wmo/wmo/wmo_scene.py
@@ -331,10 +331,10 @@ class BlenderWMOScene:
     def load_properties(self):
         """ Load global WoW WMO properties """
         properties = bpy.context.scene.wow_wmo_root
-        properties.ambient_color = (pow(self.wmo.mohd.ambient_color[2] / 255, 2.2),
-                                    pow(self.wmo.mohd.ambient_color[1] / 255, 2.2),
-                                    pow(self.wmo.mohd.ambient_color[0] / 255, 2.2),
-                                    pow(self.wmo.mohd.ambient_color[3] / 255, 2.2))
+        properties.ambient_color = ((int(self.wmo.mohd.ambient_color[2] * 255),
+                                    int(self.wmo.mohd.ambient_color[1] * 255),
+                                    int(self.wmo.mohd.ambient_color[0] * 255),
+                                    int(self.wmo.mohd.ambient_color[3] * 255))
 
         flags = set()
         if self.wmo.mohd.flags & 0x1:
